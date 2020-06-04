3dc927d809a1b0899ff64dfb4aa4510ed96bec03
anapsc <anapaula.sc10@hotmail.com>
2020-06-03 22:36:35 -0300

    Aula 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -20,6 +20,9 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${valorUF}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then(res=>res.json())
     .then(cities=>{
@@ -33,4 +36,35 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change",getCities) 
-    /*  */
+
+
+/*Itens de coleta*/
+const itemsToColllect = document.querySelectorAll(".items-grid li")
+for (const item of itemsToColllect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+//pega campo escondido
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")//adiciona ou remove classe
+
+    const itemId = itemLi.dataset.id
+
+    const jaFoiSelecionado = selectedItems.findIndex(item=>item==itemId/*função retorna true ou false*/)
+    
+    if(jaFoiSelecionado>=0){
+        //tirara seleção
+        const filteredItems = selectedItems.filter(item=>item!=itemId)
+        selectedItems = filteredItems
+    }else{
+        //se não tiver selecionado adicionar
+        selectedItems.push(itemId);
+    }    
+    //adicionar no campo escondido o item selecionado
+    collectedItems.value = selectedItems
+}
